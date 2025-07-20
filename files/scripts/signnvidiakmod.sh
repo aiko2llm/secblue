9388f694c6ece2019bfe88fd4167c467c16c1eea
aiko2llm <aiko2llm.3ot6x@passmail.net>
2025-07-20 13:36:58 +0000

    fix nvidia

--- files/scripts/signnvidiakmod.sh
+++ files/scripts/signnvidiakmod.sh
@@ -2,4 +2,21 @@
 
 set -oue pipefail
 
+KERNEL_VERSION="$(rpm -q "kernel-cachyos" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+
+dnf install -y "kernel-devel-matched-$(rpm -q 'kernel-cachyos' --queryformat '%{VERSION}')"
+dnf install -y "akmod-nvidia*.fc${RELEASE}"
+
+akmods --force --kernels "${KERNEL_VERSION}" --kmod "nvidia"
+
+# Depends on word splitting
+# shellcheck disable=SC2086
+modinfo /usr/lib/modules/${KERNEL_VERSION}/nvidia/nvidia{,-drm,-modeset,-peermem,-uvm}.ko.zst >/dev/null ||
+    (cat "/var/cache/akmods/nvidia/*.failed.log" && exit 1)
+
+# View license information
+# Depends on word splitting
+# shellcheck disable=SC2086
+modinfo -l /usr/lib/modules/${KERNEL_VERSION}/extra/nvidia/nvidia{,-drm,-modeset,-peermem,-uvm}.ko.zst
+
 ./signmodules.sh "nvidia"
