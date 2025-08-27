a91ebbdcf38d46576abaa3ed8f06dadf6a4850fe
aiko2llm <aiko2llm.3ot6x@passmail.net>
2025-08-27 03:30:07 +0000

    feat(secblue): xwayland satellite

---
+++ files/scripts/installxwaylandsatellite.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+curl -fsSL https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo -o /etc/yum.repos.d/terra.repo
+rpm-ostree install terra-release
+
+rpm-ostree install xwayland-satellite
+
+#rm -f /etc/yum.repos.d/terra.repo
