2ea3472b6b0eee47ee5dbe3874757b2fbbceb763
aiko2llm <aiko2llm.3ot6x@passmail.net>
2025-09-22 16:24:06 +0000

    chore(secblue): fix nvidia

--- files/scripts/installnvidiadriver.sh
+++ files/scripts/installnvidiadriver.sh
@@ -5,7 +5,7 @@ set -oue pipefail
 curl -fsSL https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo -o /etc/yum.repos.d/terra.repo
 rpm-ostree install terra-release
 
-rpm-ostree xorg-x11-drv-nvidia
+rpm-ostree install xorg-x11-drv-nvidia
 
 # I also want other packages distributed by terra
 #rm -f /etc/yum.repos.d/terra.repo
