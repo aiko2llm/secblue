e13732043bd286a4f45f412546afddc584d2f9ba
aiko2llm <aiko2llm.3ot6x@passmail.net>
2025-09-22 16:04:23 +0000

    fix(secblue): nvidia driver

---
+++ files/scripts/installnvidiadriver.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+curl -fsSL https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo -o /etc/yum.repos.d/terra.repo
+rpm-ostree install terra-release
+
+rpm-ostree xorg-x11-drv-nvidia
+
+# I also want other packages distributed by terra
+#rm -f /etc/yum.repos.d/terra.repo
