1337bec6d5bd7757ac5a3bfcecfefcecf6a0e9d0
aiko2llm <aiko2llm.3ot6x@passmail.net>
2025-09-24 02:49:10 +0000

    fix(secblue): temporary

--- files/scripts/signkernel.sh
+++ files/scripts/signkernel.sh
@@ -15,7 +15,8 @@
 
 set -oue pipefail
 
-KERNEL_VERSION="$(rpm -q "kernel-cachyos-lto" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+#KERNEL_VERSION="$(rpm -q "kernel-cachyos-lto" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+KERNEL_VERSION="$(rpm -q "kernel" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 
 PUBLIC_KEY_DER_PATH="../system/etc/pki/akmods/certs/akmods-secblue.der"
 PUBLIC_KEY_CRT_PATH="./certs/public_key.crt"
