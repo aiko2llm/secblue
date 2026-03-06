7a27a7d5821a34801c454de667df6470c0b6bf06
aiko2llm <aiko2llm.3ot6x@passmail.net>
2026-03-06 18:27:51 +0800

    chore: remove nvidia open

---
+++ files/scripts/removenvidiakmod.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+KERNEL_VERSION="$(rpm -q "kernel-cachyos-lto" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+
+# Depends on word splitting
+# shellcheck disable=SC2086
+rm -r /usr/lib/modules/${KERNEL_VERSION}/nvidia
