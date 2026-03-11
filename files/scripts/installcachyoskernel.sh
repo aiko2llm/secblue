7979a0ae2daae2fed452860918cf2a8f4f08a9da
aiko2llm <aiko2llm.3ot6x@passmail.net>
2026-03-11 15:34:34 +0800

    chore: kernel module

--- files/scripts/installcachyoskernel.sh
+++ files/scripts/installcachyoskernel.sh
@@ -2,7 +2,7 @@
 
 set -oue pipefail
 
-rpm-ostree initramfs --enable
+rpm-ostree initramfs --disable
 depmod -a
 rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install kernel-cachyos-lto
 rpm-ostree install scx-scheds scx-tools cachyos-settings
