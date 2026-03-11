5c64f85f076fa45d84c3e268251182aa44e1d731
aiko2llm <aiko2llm.3ot6x@passmail.net>
2026-03-11 15:29:33 +0800

    chore: kernel module

--- files/scripts/installcachyoskernel.sh
+++ files/scripts/installcachyoskernel.sh
@@ -2,10 +2,7 @@
 
 set -oue pipefail
 
-#cd /etc/yum.repos.d/
-
-#wget https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos/repo/fedora-$(rpm -E %fedora)/bieszczaders-kernel-lto-cachyos-fedora-$(rpm -E %fedora).repo
-#wget https://copr.fedorainfracloud.org/coprs/bieszczaders/kernel-cachyos-addons/repo/fedora-$(rpm -E %fedora)/bieszczaders-kernel-cachyos-addons-fedora-$(rpm -E %fedora).repo
-
+rpm-ostree initramfs --enable
+depmod -a
 rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install kernel-cachyos-lto
 rpm-ostree install scx-scheds scx-tools cachyos-settings
